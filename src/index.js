dc8549b226cf0ffe09ff858029cdec06aaa53fdb
Elena Signore <elenasignore@MacBook-Pro-di-Elena.local>
2021-03-03 15:37:34 +0100

    creato progetto prova base React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+/** importiamo la libreria 'react', quindi i suoi metodi, e accediamo a questa libreria attrav erso la variabile React*/
+import React from 'react';
+
+
+/**
+ * React.createElement(
+ * type,
+ * [props],
+ * [...children]
+ * )
+ */
+const element = React.createElement('h1')
